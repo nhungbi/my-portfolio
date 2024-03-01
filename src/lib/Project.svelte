e9892ae9f62f652a950b95e4d007cef5b48b46fa
nhungbi <nhungvan16@gmail.com>
2024-03-01 12:58:41 -0500

    fned lab 5

---
+++ src/lib/Project.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let info = {};
+    let p = info;
+    export let hLevel = 2;
+</script>
+<style>
+    article {
+        display: grid;
+        grid-template-rows: subgrid;
+        grid-row: span 3;
+    }
+
+    h2 {
+        margin: 0;
+    }
+    h3 {
+        margin: 0;
+    }
+
+
+</style>
+<article>
+    <svelte:element this={"h" + hLevel}>{ p.title }</svelte:element>
+    <img src={p.image} alt="">
+    <p>{p.description}</p>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script> 
+    import projects from '$lib/projects.json';
+    import Project from "$lib/Project.svelte";
+</script>
 <h1>Nhung Van</h1>
 <p>I'm a junior studying CS and AI.</p>
 <img class = 'panda-img' src="./images/panda.png" alt="A picture of a panda.">
+<h2> Latest Projects </h2>
+<div class="projects highlights">
+    {#each projects.splice(0,3) as p}
+        <Project info={p} hLevel=3 />
+    {/each}
+</div>
